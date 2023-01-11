a5f13c4c634ccae6f55285f7f1dae2953eb38f63
alastairkitchen <ali7566@gmail.com>
2023-01-11 16:14:19 +0000

    fix warning noreferer

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -21,7 +21,7 @@ function Home() {
 				<div className="site-content__container">
 					<p>Welcome to the rick and morty webapp</p>
 					<p>Search and view information about characters on the show in the characters section and view episodes information in the episodes section.</p>
-					<p>This site was built as a personal project using the open source <a target='_blank' rel="noreferrer" href='https://rickandmortyapi.com/'>Rick and Morty api</a>, Kudos to <a target='_blank' href="https://github.com/afuh">Axel Fuhrmann</a> for doing a fantastic job!</p>
+					<p>This site was built as a personal project using the open source <a target='_blank' rel="noreferrer" href='https://rickandmortyapi.com/'>Rick and Morty api</a>, Kudos to <a target='_blank' rel="noreferrer" href="https://github.com/afuh">Axel Fuhrmann</a> for doing a fantastic job!</p>
 					<p>I want to make it clear that none of the content or images are owned by me and are soley used as a means of showcasing an app build.</p>
 				</div>
 			</div>
